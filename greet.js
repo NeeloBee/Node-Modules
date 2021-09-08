88c6cf0d98fe1db6c029a6dc7f2eb4cf741c4d46
NeeloBee <boineelolk@gmail.com>
2021-09-08 17:43:04 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,17 @@
+/*function greet(neighboursName) {
+    return "Hello, " + neighboursName;
+  }
+  
+  console.log(greet);
+
+  var msg = greet('Xola');
+  console.log(msg);
+
+*/
+
+module.exports = function(name) {
+    return 'Hello, ' + name;
+
+}
+
+
